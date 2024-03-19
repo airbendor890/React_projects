47ee84775904e60be44fa7800e4dc382c47aee28
Haraprasad Dhal <airbendor890@gmail.com>
2024-03-19 23:40:54 +0530

    Add files via upload

---
+++ survey_app_project/src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(){
+    return(
+        <div className='text-center qform'>
+            <h3>About Section</h3>
+            <p>This survey website is made using React.</p>
+        </div>
+    )
+}
+
+export  default About;
