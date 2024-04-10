10369f5311c3456c28f94f7b2361bd876248096d
Haraprasad Dhal <airbendor890@gmail.com>
2024-04-10 16:17:15 +0530

    Add files via upload

---
+++ keeper-app-part/src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "Notes on programming",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    key: 2,
+    title: "shower thoughts",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    key: 3,
+    title: "feed fish",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    key: 4,
+    title: "Hardware",
+    content:
+      "Hi this is a note on hardware"
+  },
+  {
+    key: 5,
+    title: "Big Data",
+    content: "Exponential rise in amount of data"
+  }
+];
+
+export default notes;
